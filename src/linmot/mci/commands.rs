c73d8be6fa5ca58b8067762e2bd16b5fbd4404d4
Erin <fenbyfluid@gmail.com>
2025-12-01 00:04:36 +0000

    Implement simple CLI interface

--- src/linmot/mci/commands.rs
+++ src/linmot/mci/commands.rs
@@ -13,6 +13,45 @@ pub enum Command {
         acceleration: Acceleration,
         deceleration: Acceleration,
     },
+    VaiIncrementDemPos {
+        position_increment: Position,
+        maximal_velocity: Velocity,
+        acceleration: Acceleration,
+        deceleration: Acceleration,
+    },
+    VaiStop {
+        deceleration: Acceleration,
+    },
+    PStreamWithDriveGeneratedTimeStamp {
+        position: Position,
+    },
+    PvStreamWithDriveGeneratedTimeStamp {
+        position: Position,
+        velocity: Velocity,
+    },
+    PStreamWithDriveGeneratedTimeStampAndConfiguredPeriodTime {
+        position: Position,
+    },
+    PvStreamWithDriveGeneratedTimeStampAndConfiguredPeriodTime {
+        position: Position,
+        velocity: Velocity,
+    },
+    PvaStreamWithDriveGeneratedTimeStamp {
+        position: Position,
+        velocity: Velocity,
+        acceleration: Acceleration,
+    },
+    PvaStreamWithDriveGeneratedTimeStampAndConfiguredPeriodTime {
+        position: Position,
+        velocity: Velocity,
+        acceleration: Acceleration,
+    },
+    PvaStreamWithControllerGeneratedTimeStamp {
+        position: Position,
+        velocity: Velocity,
+        acceleration: Acceleration,
+    },
+    StopStream,
 }
 
 impl Command {
@@ -21,10 +60,21 @@ impl Command {
         match self {
             Self::NoOperation => 0x000,
             Self::VaiGoToPos { .. } => 0x010,
+            Self::VaiIncrementDemPos { .. } => 0x011,
+            Self::VaiStop { .. } => 0x017,
+            Self::PStreamWithDriveGeneratedTimeStamp { .. } => 0x030,
+            Self::PvStreamWithDriveGeneratedTimeStamp { .. } => 0x031,
+            Self::PStreamWithDriveGeneratedTimeStampAndConfiguredPeriodTime { .. } => 0x032,
+            Self::PvStreamWithDriveGeneratedTimeStampAndConfiguredPeriodTime { .. } => 0x033,
+            Self::PvaStreamWithDriveGeneratedTimeStamp { .. } => 0x034,
+            Self::PvaStreamWithDriveGeneratedTimeStampAndConfiguredPeriodTime { .. } => 0x035,
+            Self::PvaStreamWithControllerGeneratedTimeStamp { .. } => 0x03A,
+            Self::StopStream => 0x03F,
         }
     }
 
     pub(super) fn write_parameters(&self, w: &mut Writer) -> Result<()> {
+        #[expect(clippy::match_same_arms)]
         match self {
             Self::NoOperation => {}
             Self::VaiGoToPos { target_position, maximal_velocity, acceleration, deceleration } => {
@@ -33,6 +83,45 @@ impl Command {
                 acceleration.write_to(w)?;
                 deceleration.write_to(w)?;
             }
+            Self::VaiIncrementDemPos { position_increment, maximal_velocity, acceleration, deceleration } => {
+                position_increment.write_to(w)?;
+                maximal_velocity.write_to(w)?;
+                acceleration.write_to(w)?;
+                deceleration.write_to(w)?;
+            }
+            Self::VaiStop { deceleration } => {
+                deceleration.write_to(w)?;
+            }
+            Self::PStreamWithDriveGeneratedTimeStamp { position } => {
+                position.write_to(w)?;
+            }
+            Self::PvStreamWithDriveGeneratedTimeStamp { position, velocity } => {
+                position.write_to(w)?;
+                velocity.write_to(w)?;
+            }
+            Self::PStreamWithDriveGeneratedTimeStampAndConfiguredPeriodTime { position } => {
+                position.write_to(w)?;
+            }
+            Self::PvStreamWithDriveGeneratedTimeStampAndConfiguredPeriodTime { position, velocity } => {
+                position.write_to(w)?;
+                velocity.write_to(w)?;
+            }
+            Self::PvaStreamWithDriveGeneratedTimeStamp { position, velocity, acceleration } => {
+                position.write_to(w)?;
+                velocity.write_to(w)?;
+                acceleration.write_to(w)?;
+            }
+            Self::PvaStreamWithDriveGeneratedTimeStampAndConfiguredPeriodTime { position, velocity, acceleration } => {
+                position.write_to(w)?;
+                velocity.write_to(w)?;
+                acceleration.write_to(w)?;
+            }
+            Self::PvaStreamWithControllerGeneratedTimeStamp { position, velocity, acceleration } => {
+                position.write_to(w)?;
+                velocity.write_to(w)?;
+                acceleration.write_to(w)?;
+            }
+            Self::StopStream => {}
         }
 
         Ok(())
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use linmot::mci::units::{Acceleration, Position, Velocity};
 use linmot::mci::{Command, ControlFlags, ErrorCode, MotionCommand, State};
 use linmot::udp::{BUFFER_SIZE, CONTROLLER_PORT, DRIVE_PORT, Request, Response, ResponseFlags};
 use std::net::{Ipv4Addr, UdpSocket};
-use std::thread::sleep;
+use std::sync::mpsc;
 use std::time::{Duration, Instant};
 
 pub mod linmot;
@@ -14,15 +14,150 @@ mod writer;
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Options {
+    /// Drive controller hostname or IP address
     drive_address: String,
+    /// Loop interval in milliseconds
+    #[clap(short, long, default_value = "5")]
+    loop_interval: u64,
+    /// Report interval in milliseconds
+    #[clap(short, long, default_value = "1000")]
+    report_interval: u64,
 }
 
 fn main() -> Result<()> {
     let options = Options::parse();
 
-    DriveConnection::new(&options.drive_address)?.start_loop().context("Failed to connect to drive")?;
+    let (stroke_params_sender, stroke_params_receiver) = mpsc::channel();
+
+    std::thread::spawn(move || {
+        run_input_loop(stroke_params_sender);
+    });
+
+    DriveConnection::new(&options.drive_address, stroke_params_receiver)?
+        .start_loop(Duration::from_millis(options.loop_interval), Duration::from_millis(options.report_interval))
+        .context("Failed to connect to drive")?;
 
     Ok(())
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum StrokeMode {
+    Uncontrolled,
+    Stopped,
+    Active,
+}
+
+#[derive(Debug, Clone)]
+struct StrokeParams {
+    mode: StrokeMode,
+    start: Position,
+    length: Position,
+    direction_change_tolerance: Position,
+    forwards_velocity: Velocity,
+    forwards_acceleration: Acceleration,
+    forwards_deceleration: Acceleration,
+    backwards_velocity: Velocity,
+    backwards_acceleration: Acceleration,
+    backwards_deceleration: Acceleration,
+}
+
+impl StrokeParams {
+    const fn new() -> Self {
+        Self {
+            mode: StrokeMode::Uncontrolled,
+            start: Position::from_millimeters(0),
+            length: Position::from_millimeters(0),
+            direction_change_tolerance: Position::from_millimeters(1),
+            forwards_velocity: Velocity::from_meters_per_second(1),
+            forwards_acceleration: Acceleration::from_meters_per_second_squared(1),
+            forwards_deceleration: Acceleration::from_meters_per_second_squared(1),
+            backwards_velocity: Velocity::from_meters_per_second(1),
+            backwards_acceleration: Acceleration::from_meters_per_second_squared(1),
+            backwards_deceleration: Acceleration::from_meters_per_second_squared(1),
+        }
+    }
+}
+
+fn run_input_loop(stroke_params_sender: mpsc::Sender<StrokeParams>) {
+    let mut input = String::new();
+    let mut stroke_params = StrokeParams::new();
+
+    loop {
+        input.clear();
+        std::io::stdin().read_line(&mut input).unwrap();
+
+        let (command, value) = match input.split_once(' ') {
+            Some((command, value)) => (command, value.trim_end().parse().ok()),
+            None => (input.trim_end(), None),
+        };
+
+        match (command, value) {
+            ("h", _) => {
+                println!("Available commands:");
+                println!("   p = Toggle power (hard stop)");
+                println!("   f = Toggle soft stop");
+                println!("   r = Reset parameters to default");
+                println!("   s = Set stroke start position in mm");
+                println!("   l = Set stroke length in mm");
+                println!("   t = Set direction change tolerance in mm");
+                println!("   v = Set velocity in m/s");
+                println!("   a = Set acceleration in m/s²");
+                println!("  fv = Set forwards velocity in m/s");
+                println!("  fa = Set forwards acceleration in m/s²");
+                println!("  fd = Set forwards deceleration in m/s²");
+                println!("  bv = Set backwards velocity in m/s");
+                println!("  ba = Set backwards acceleration in m/s²");
+                println!("  bd = Set backwards deceleration in m/s²");
+            }
+            ("f", _) => {
+                stroke_params.mode = match stroke_params.mode {
+                    StrokeMode::Active => StrokeMode::Stopped,
+                    StrokeMode::Stopped => StrokeMode::Active,
+                    mode => mode,
+                }
+            }
+            ("r", _) => stroke_params = StrokeParams { mode: stroke_params.mode, ..StrokeParams::new() },
+            ("p", _) => {
+                stroke_params.mode = match stroke_params.mode {
+                    StrokeMode::Uncontrolled => StrokeMode::Active,
+                    _ => StrokeMode::Uncontrolled,
+                }
+            }
+            ("s", Some(v)) => stroke_params.start = Position::from_millimeters_f64(v),
+            ("l", Some(v)) => stroke_params.length = Position::from_millimeters_f64(v),
+            ("t", Some(v)) => stroke_params.direction_change_tolerance = Position::from_millimeters_f64(v),
+            ("v", Some(v)) => {
+                stroke_params.forwards_velocity = Velocity::from_meters_per_second_f64(v);
+                stroke_params.backwards_velocity = stroke_params.forwards_velocity;
+            }
+            ("a", Some(v)) => {
+                stroke_params.forwards_acceleration = Acceleration::from_meters_per_second_squared_f64(v);
+                stroke_params.forwards_deceleration = stroke_params.forwards_acceleration;
+                stroke_params.backwards_acceleration = stroke_params.forwards_acceleration;
+                stroke_params.backwards_deceleration = stroke_params.backwards_acceleration;
+            }
+            ("fv", Some(v)) => stroke_params.forwards_velocity = Velocity::from_meters_per_second_f64(v),
+            ("fa", Some(v)) => {
+                stroke_params.forwards_acceleration = Acceleration::from_meters_per_second_squared_f64(v)
+            }
+            ("fd", Some(v)) => {
+                stroke_params.forwards_deceleration = Acceleration::from_meters_per_second_squared_f64(v)
+            }
+            ("bv", Some(v)) => stroke_params.backwards_velocity = Velocity::from_meters_per_second_f64(v),
+            ("ba", Some(v)) => {
+                stroke_params.backwards_acceleration = Acceleration::from_meters_per_second_squared_f64(v)
+            }
+            ("bd", Some(v)) => {
+                stroke_params.backwards_deceleration = Acceleration::from_meters_per_second_squared_f64(v)
+            }
+            _ => {
+                println!("Unknown command or missing value, use 'h' for help");
+                continue;
+            }
+        }
+
+        stroke_params_sender.send(stroke_params.clone()).unwrap();
+    }
 }
 
 struct DriveConnection {
@@ -32,12 +167,13 @@ struct DriveConnection {
     last_state: State,
     control_flags: ControlFlags,
     acknowledge_error: bool,
-    pending_command_count: Option<u8>,
-    pending_motion_command: Option<Command>,
+    moving_forwards: bool,
+    stroke_params: StrokeParams,
+    stroke_params_receiver: mpsc::Receiver<StrokeParams>,
 }
 
 impl DriveConnection {
-    fn new(address: &str) -> Result<Self> {
+    fn new(address: &str, stroke_params_receiver: mpsc::Receiver<StrokeParams>) -> Result<Self> {
         let socket = UdpSocket::bind((Ipv4Addr::UNSPECIFIED, CONTROLLER_PORT))?;
         socket.connect((address, DRIVE_PORT))?;
 
@@ -48,19 +184,58 @@ impl DriveConnection {
             buffer: [0u8; BUFFER_SIZE],
             last_response: None,
             last_state: State::NotReadyToSwitchOn,
-            control_flags: ControlFlags::SWITCH_ON,
+            control_flags: ControlFlags::empty(),
             acknowledge_error: true,
-            pending_command_count: None,
-            pending_motion_command: Some(Command::VaiGoToPos {
-                target_position: Position::from_millimeters(150),
-                maximal_velocity: Velocity::from_meters_per_second(1),
-                acceleration: Acceleration::from_meters_per_second_squared(1),
-                deceleration: Acceleration::from_meters_per_second_squared(1),
-            }),
+            moving_forwards: false,
+            stroke_params: StrokeParams::new(),
+            stroke_params_receiver,
         })
     }
 
+    fn get_motion_command_for_stroke_params(
+        params: &StrokeParams,
+        moving_forwards: &mut bool,
+        demand_position: &Position,
+    ) -> Command {
+        if *moving_forwards {
+            if params.mode == StrokeMode::Stopped {
+                return Command::VaiStop { deceleration: params.forwards_deceleration };
+            }
+
+            if *demand_position >= (params.start + params.length) - params.direction_change_tolerance {
+                *moving_forwards = false;
+            }
+
+            Command::VaiGoToPos {
+                target_position: params.start + params.length,
+                maximal_velocity: params.forwards_velocity,
+                acceleration: params.forwards_acceleration,
+                deceleration: params.forwards_deceleration,
+            }
+        } else {
+            if params.mode == StrokeMode::Stopped {
+                return Command::VaiStop { deceleration: params.backwards_deceleration };
+            }
+
+            if *demand_position <= params.start + params.direction_change_tolerance {
+                *moving_forwards = true;
+            }
+
+            Command::VaiGoToPos {
+                target_position: params.start,
+                maximal_velocity: params.backwards_velocity,
+                acceleration: params.backwards_acceleration,
+                deceleration: params.backwards_deceleration,
+            }
+        }
+    }
+
     fn loop_tick(&mut self) -> Result<()> {
+        // Check for new stroke parameters — keep the latest if multiple are pending
+        while let Ok(new_params) = self.stroke_params_receiver.try_recv() {
+            self.stroke_params = new_params;
+        }
+
         let mut request = Request {
             response_flags: ResponseFlags::STATUS_FLAGS
                 | ResponseFlags::STATE
@@ -74,9 +249,10 @@ impl DriveConnection {
 
         // TODO: We currently have several control bits forced in the parameter configuration,
         //       re-evaluate if we want to implement the full state machine instead.
-        if let Some(Response { state: Some(state), .. }) = &self.last_response {
+        if let Some(Response { state: Some(state), demand_position: Some(demand_position), .. }) = &self.last_response {
             if state != &self.last_state {
-                println!("Transitioned from {:?} to {:?}", self.last_state, state);
+                // TODO: Figure out how to ignore OperationEnabled->OperationEnabled transitions if only motion_command_count changed.
+                // println!("Transitioned from {:?} to {:?}", self.last_state, state);
                 self.last_state = *state;
             }
 
@@ -85,8 +261,13 @@ impl DriveConnection {
                     self.control_flags = ControlFlags::empty();
                 }
                 State::ReadyToSwitchOn => {
+                    // We only acknowledge an error once on startup to get the drive into a stable state.
+                    // Require user confirmation before acknowledging any drive errors during operation.
                     self.acknowledge_error = false;
-                    self.control_flags = ControlFlags::SWITCH_ON;
+
+                    if self.stroke_params.mode != StrokeMode::Uncontrolled {
+                        self.control_flags = ControlFlags::SWITCH_ON;
+                    }
                 }
                 State::Error { error_code } if self.acknowledge_error => {
                     println!("Acknowledging error: {error_code:?}");
@@ -96,44 +277,19 @@ impl DriveConnection {
                 State::OperationEnabled { homed: false, .. } => {
                     self.control_flags.insert(ControlFlags::HOME);
                 }
-                State::OperationEnabled { motion_active: false, motion_command_count, .. } => {
-                    if let Some(pending_count) = self.pending_command_count
-                        && pending_count == *motion_command_count
-                    {
-                        println!("Motion command {pending_count} complete");
+                State::OperationEnabled { homed: true, motion_command_count, .. } => {
+                    let next_command_count = (motion_command_count.wrapping_add(1)) & 0xF;
 
-                        self.pending_command_count = None;
-                        // self.pending_motion_command = None;
+                    if self.stroke_params.mode == StrokeMode::Uncontrolled {
+                        self.control_flags.remove(ControlFlags::SWITCH_ON);
+                    } else {
+                        let command = Self::get_motion_command_for_stroke_params(
+                            &self.stroke_params,
+                            &mut self.moving_forwards,
+                            demand_position,
+                        );
 
-                        // TODO: Quick oscillating test.
-                        self.pending_motion_command =
-                            if let Some(Command::VaiGoToPos { target_position, .. }) = self.pending_motion_command {
-                                let new_target_position = if target_position < Position::from_millimeters(150) {
-                                    Position::from_millimeters(200)
-                                } else {
-                                    Position::from_millimeters(100)
-                                };
-
-                                Some(Command::VaiGoToPos {
-                                    target_position: new_target_position,
-                                    maximal_velocity: Velocity::from_meters_per_second(1),
-                                    acceleration: Acceleration::from_meters_per_second_squared(1),
-                                    deceleration: Acceleration::from_meters_per_second_squared(1),
-                                })
-                            } else {
-                                None
-                            }
-                    }
-
-                    if let Some(pending_command) = self.pending_motion_command {
-                        // TODO: We had a case where the received command count was 0, but internally it had 1 stored, and it ignored our new command.
-                        let next_command_count = (motion_command_count + 2) % 0xF;
-                        self.pending_command_count = Some(next_command_count);
-
-                        request.motion_command =
-                            Some(MotionCommand { count: next_command_count, command: pending_command });
-
-                        println!("Sending motion command {next_command_count}: {pending_command:?}");
+                        request.motion_command = Some(MotionCommand { count: next_command_count, command });
                     }
                 }
                 State::Homing { finished: true } => {
@@ -159,65 +315,74 @@ impl DriveConnection {
         Ok(())
     }
 
-    fn start_loop(&mut self) -> Result<()> {
-        const LOOP_INTERVAL: Duration = Duration::from_millis(5);
-        const REPORT_INTERVAL: Duration = Duration::from_secs(1);
-
-        println!("Loop interval: {LOOP_INTERVAL:?}");
-
-        self.socket.set_read_timeout(Some(LOOP_INTERVAL / 2))?;
+    fn start_loop(&mut self, loop_interval: Duration, report_interval: Duration) -> Result<()> {
+        self.socket.set_read_timeout(Some(loop_interval / 2))?;
 
         let mut last_loop_report = Instant::now();
         let mut loop_duration_sum = Duration::ZERO;
-        let mut loop_message_count: u32 = 0;
-        let mut loop_error_count: u32 = 0;
+        let mut loop_duration_min = Duration::MAX;
+        let mut loop_duration_max = Duration::ZERO;
+        let mut loop_message_count: usize = 0;
+        let mut loop_error_history = Vec::new();
 
-        let mut next_tick = Instant::now() + LOOP_INTERVAL;
+        let mut next_tick = Instant::now() + loop_interval;
 
         loop {
             let iter_start = Instant::now();
 
             if let Err(error) = self.loop_tick() {
-                eprintln!("Error in loop tick: {error}");
-                loop_error_count += 1;
+                // TODO: Print the error if it's not just a read timeout
+                loop_error_history.push(error);
             }
 
             loop_message_count += 1;
 
             let loop_duration = iter_start.elapsed();
             loop_duration_sum += loop_duration;
+            loop_duration_min = loop_duration_min.min(loop_duration);
+            loop_duration_max = loop_duration_max.max(loop_duration);
 
-            if last_loop_report.elapsed() >= REPORT_INTERVAL {
-                if loop_error_count > 0 && loop_error_count == loop_message_count {
-                    break Err(anyhow!("Too many errors in loop, aborting"));
-                }
+            if last_loop_report.elapsed() >= report_interval {
+                println!();
 
-                let avg_loop_duration = loop_duration_sum / loop_message_count;
+                // TODO: Print the error history in a compact format
+                let avg_loop_duration = loop_duration_sum / (loop_message_count as u32);
                 println!(
-                    "Average loop duration: {:?} ({:.2}% usage, {}/{} errors)",
+                    "Timing statistics: {:?} average, {:?} min, {:?} max, {:.2}% usage ({:.2}% peak), {}/{} errors",
                     avg_loop_duration,
-                    (avg_loop_duration.as_secs_f64() / LOOP_INTERVAL.as_secs_f64()) * 100.0,
-                    loop_error_count,
+                    loop_duration_min,
+                    loop_duration_max,
+                    (avg_loop_duration.as_secs_f64() / loop_interval.as_secs_f64()) * 100.0,
+                    (loop_duration_max.as_secs_f64() / loop_interval.as_secs_f64()) * 100.0,
+                    loop_error_history.len(),
                     loop_message_count,
                 );
 
                 self.print_drive_status();
 
+                println!("{:#?}", self.stroke_params);
+
+                if !loop_error_history.is_empty() && loop_error_history.len() == loop_message_count {
+                    break Err(anyhow!("Too many errors in loop, aborting"));
+                }
+
                 last_loop_report = Instant::now();
                 loop_duration_sum = Duration::ZERO;
+                loop_duration_min = Duration::MAX;
+                loop_duration_max = Duration::ZERO;
                 loop_message_count = 0;
-                loop_error_count = 0;
+                loop_error_history.clear();
             }
 
             // Sleep until the next tick; if overrun, report lateness and realign to the next interval boundary
             let now = Instant::now();
             if let Some(remaining) = next_tick.checked_duration_since(now) {
-                sleep(remaining);
-                next_tick += LOOP_INTERVAL;
+                std::thread::sleep(remaining);
+                next_tick += loop_interval;
             } else {
                 let late_by = now.duration_since(next_tick);
                 eprintln!("Late by {late_by:?}");
-                next_tick = now + LOOP_INTERVAL;
+                next_tick = now + loop_interval;
             }
         }
     }
